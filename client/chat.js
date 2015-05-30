890bee67457942b95f81375b3e6fab2092615c64
Franco Catena <francocatena@gmail.com>
2015-05-30 12:45:08 -0300

    Clase 2

---
+++ client/chat.js
@@ -0,0 +1,27 @@
+Meteor.subscribe('mensajes')
+
+Template.chat.helpers({
+  mensajes: function () {
+    return Mensajes.find({}, { sort: { fecha: 1 } })
+  }
+})
+
+Template.chat.events({
+  'submit': function (event) {
+    event.preventDefault()
+
+    var $mensaje = $('#mensaje')
+    var texto    = $mensaje.val().trim()
+
+    if (texto)
+      Mensajes.insert({ texto: texto, fecha: new Date })
+
+    $mensaje.val('').focus()
+  }
+})
+
+Template.mensaje.helpers({
+  fecha: function () {
+    return moment(this.fecha).format('LT')
+  }
+})
